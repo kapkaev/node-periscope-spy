7342015cce923f7f634d28d08191cae6840a8192
Ildar Kapkaev <kirs.box@gmail.com>
2015-12-06 22:07:00 -0800

    subscribe to stream & write to file

--- spy.js
+++ spy.js
@@ -1,46 +1,92 @@
+var fs = require('fs');
 var Twit = require('twit');
-var periscope = require('node-periscope-stream');
 var peristream = require('peristream');
+
+PERISCOPE_ID_REG = /periscope.tv\/w\/(.*)/i
 
 function PeriscopeSpy(config) {
 	this.T = new Twit(config);
-
-  this.getUserId = function(userName) {
-    this.getUserData(userName).id;
-  }
-
-  this.getUserData = function (userName) {
-    this.T.get('users/lookup', { screen_name: userName }, function(err, data, response) {
-      if(err) {
-        console.log(err);
-        return {};
-      }
-      return data;
-    });
-  }
+  this.session = new Date().getTime();
 
 }
 
-PeriscopeSpy.prototype.start = function(userName) {
-  var userId = this.getUserId(userName);
-	var stream = this.T.stream('statuses/filter', { follow: [userId] });
+PeriscopeSpy.prototype.follow = function(userId) {
+  var stream = this.T.stream('statuses/filter', { track: 'periscope' })
 
-	stream.on('tweet', function (tweet) {
-		if (!tweet.entities.urls || !tweet.entities.urls.length) {
-			return;
-		}
+  stream.on('tweet', function (tweet) {
+    if (!tweet.entities.urls || !tweet.entities.urls.length) {
+      return;
+    }
 
-		var periscopeUrl = tweet.entities.urls[0].expanded_url;
+    var url = tweet.entities.urls[0].expanded_url;
 
-		periscope(periscopeUrl, function(err, info) {
-			if (err) {
-				console.log(err);
-				return;
-			}
+    // skip non periscope links
+    if (!url.match(PERISCOPE_ID_REG)){
+      return;
+    }
 
-      console.log(info)
-		});
-	});
+    var streamId = url.match(PERISCOPE_ID_REG)[1];
+
+    this.subscribeToStream(streamId);
+  }.bind(this));
+
+  var log = function(name, msg) {
+    console.log("name: " + name);
+    if (name == 'error'){
+      console.log(msg);
+    }
+  }
+
+  stream.on('limit', function (limitMessage) {
+    log('limit', limitMessage);
+  })
+
+  stream.on('error', function (eventMsg) {
+    log('error', eventMsg);
+  })
 };
+
+
+PeriscopeSpy.prototype.subscribeToStream = function(streamId){
+  var stream = peristream(streamId);
+
+  var log = function(name, msg) {
+    console.log(name, msg);
+    this.saveToFile(msg);
+  }.bind(this)
+
+  stream.connect().then(function(emitter){
+    emitter.on(peristream.ALL, function(message){
+      log('ALL', message)
+    });
+
+    emitter.on(peristream.HEARTS, function(message){
+      log('HEARTS', message)
+    });
+
+    emitter.on(peristream.COMMENTS, function(message){
+      log('COMMENTS', message)
+    });
+
+    emitter.on(peristream.DISCONNECT, function(message){
+      log('DISCONNECT', message)
+    });
+
+    emitter.on(peristream.JOINED, function(message){
+      log('JOINED', message)
+    });
+
+  });
+
+};
+
+PeriscopeSpy.prototype.saveToFile = function(message) {
+  var data = JSON.stringify(message, null, 2)
+  // events types
+  // COMMENTS = 1;
+  // HEARTS = 2;
+  // JOINED = 3;
+  fs.appendFile(this.session + ".json", data);
+}
 
 module.exports = PeriscopeSpy;
