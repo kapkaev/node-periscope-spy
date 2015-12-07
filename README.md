a67dcc76957583b0993a3e37d4fd9eaeebff38b2
Ildar Kapkaev <kirs.box@gmail.com>
2015-12-06 18:45:17 -0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# node-periscope-recorder
+Get Periscope lives from Twitter and store data from them
+
+## Install
+```js
+npm install node-periscope-spy
+```
+
+## Use
+```js
+var Spy = require('node-periscope-spy');
+var config = {
+	consumer_key: '',
+	consumer_secret: '',
+	access_token: '',
+	access_token_secret: ''
+};
+var rec = new Spy(config);
+
+var userId = '30634464';
+spy.start(userId);
+```
---
+++ spy.js
@@ -0,0 +1,46 @@
+var Twit = require('twit');
+var periscope = require('node-periscope-stream');
+var peristream = require('peristream');
+
+function PeriscopeSpy(config) {
+	this.T = new Twit(config);
+
+  this.getUserId = function(userName) {
+    this.getUserData(userName).id;
+  }
+
+  this.getUserData = function (userName) {
+    this.T.get('users/lookup', { screen_name: userName }, function(err, data, response) {
+      if(err) {
+        console.log(err);
+        return {};
+      }
+      return data;
+    });
+  }
+
+}
+
+PeriscopeSpy.prototype.start = function(userName) {
+  var userId = this.getUserId(userName);
+	var stream = this.T.stream('statuses/filter', { follow: [userId] });
+
+	stream.on('tweet', function (tweet) {
+		if (!tweet.entities.urls || !tweet.entities.urls.length) {
+			return;
+		}
+
+		var periscopeUrl = tweet.entities.urls[0].expanded_url;
+
+		periscope(periscopeUrl, function(err, info) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+
+      console.log(info)
+		});
+	});
+};
+
+module.exports = PeriscopeSpy;
